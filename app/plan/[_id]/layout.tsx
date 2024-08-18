77d263d1d6884220558681f3b41ea306791ef4c1
Wei-Yao Wang <z5n9800z@gmail.com>
2024-08-18 18:20:10 +0800

    fix:出發時間選擇PM12:00會判定為24:00的問題

--- app/plan/[_id]/layout.tsx
+++ app/plan/[_id]/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import Header from "@/components/Header/HeaderWider";
 
 export const metadata: Metadata = {
-  title: "規劃行程 | Good to Go",
+  title: "規劃行程 | Good to GO",
   description: "開始規劃行程",
 };
 
--- app/trips/layout.tsx
+++ app/trips/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import Header from "@/components/Header/Header";
 
 export const metadata: Metadata = {
-  title: "我的行程 | Good to Go",
+  title: "我的行程 | Good to GO",
   description: "我的行程規劃",
 };
 
