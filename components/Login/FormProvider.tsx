3d40ea0df4773022258aae5c693d7bc33b084201
Wei-Yao Wang <z5n9800z@gmail.com>
2024-09-06 00:06:34 +0800

    修改樣式、增加圖標、首頁功能介紹區塊

--- components/Login/FormProvider.tsx
+++ components/Login/FormProvider.tsx
@@ -6,7 +6,7 @@ import SignUpCard from "@/components/Login/SignUpCard";
 const FormProvider = () => {
   const [state, setState] = useState(true);
   return (
-    <div className="relative z-10 flex min-h-[455px] min-w-[380px] flex-col items-center rounded-xl border border-solid border-black bg-zinc-100/80 p-10 shadow-2xl">
+    <div className="relative z-10 flex min-h-[455px] min-w-[380px] flex-col items-center rounded-xl bg-zinc-100/80 p-10 shadow-xl">
       {state ? (
         <SignInCard switchToSignUp={setState} />
       ) : (
