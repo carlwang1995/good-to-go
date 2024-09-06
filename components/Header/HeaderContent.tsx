76c89edd54605d4970482791eda83acc83bd19f9
Wei-Yao Wang <z5n9800z@gmail.com>
2024-09-06 14:45:45 +0800

    新增網頁logo、更新字體；新增404 page

--- components/Header/HeaderContent.tsx
+++ components/Header/HeaderContent.tsx
@@ -1,13 +1,28 @@
 import Link from "next/link";
 import React from "react";
 import UserNav from "./UserNav";
+import Image from "next/image";
+import { Alkatra, Mochiy_Pop_P_One } from "next/font/google";
+
+const fontStyle = Alkatra({ weight: "700", subsets: ["latin"] });
 
 const HeaderContent = () => {
   return (
     <>
       <Link style={{ textDecoration: "none" }} href="/">
         <span className="flex h-full items-center justify-center pl-4">
-          <p className="text-3xl font-extrabold text-sky-800">Good to GO</p>
+          <Image
+            src="/world.png"
+            alt="logo"
+            height={36}
+            width={36}
+            className="mr-2"
+          />
+          <p
+            className={`${fontStyle.className} font-serif text-3xl text-sky-800`}
+          >
+            Good to GO
+          </p>
         </span>
       </Link>
       <UserNav />
